ecc247d8d2abc1cb1ed343fa9280658ff8844568
Christian Oliveros <christianol_01@hotmail.com>
2020-12-28 14:19:33 -0400

    More Distributions

--- distributions.py
+++ distributions.py
@@ -3,15 +3,15 @@ import random
 
 import numpy as np
 
-def generateUniformPoints(N):
+def generateUniformDistribution(N):
     points = [None] * N
     for i in range(N):
         points[i] = list(np.random.rand(1, 2)[0])
     return points
 
-UniformDistribution = ("Uniform Distribution", generateUniformPoints)
+UniformDistribution = ("Uniform Distribution", generateUniformDistribution)
 
-def generateBoxPoints(N):
+def generateBoxDistribution(N):
     points = [None] * N
     for i in range(N-4):
         points[i] = list(np.random.rand(1, 2)[0])
@@ -22,7 +22,7 @@ def generateBoxPoints(N):
     random.shuffle(points)
     return points
 
-BoxDistribution = ("Box Distribution", generateBoxPoints)
+BoxDistribution = ("Box Distribution", generateBoxDistribution)
 
 def generateLineDistributionOrdered(N):
     points = [None] * N
@@ -47,20 +47,48 @@ def generateLineDistributionShuffled(N):
 
 LineShuffledDistribution = ("Line Shuffled Distribution", generateLineDistributionShuffled)
 
-def generateLog2Distribution(N):
+def generateLog2DistributionOrdered(N):
     points = [None] * N
     for i in range(N-1):
         points[i] = [i+1, math.log2(i+1)]
     points[N-1] = [N, 0]
     return points
 
-Log2Distribution = ("Log2 Distribution", generateLog2Distribution)
+Log2OrderedDistribution = ("Log2 Ordered Distribution", generateLog2DistributionOrdered)
 
-def generateSquaredDistribution(N):
+def generateLog2DistributionInverted(N):
+    points = generateLog2DistributionOrdered(N)
+    points.reverse()
+    return points
+
+Log2InvertedDistribution = ("Log2 Inverted Distribution", generateLog2DistributionInverted)
+
+def generateLog2DistributionShuffled(N):
+    points = generateLog2DistributionOrdered(N)
+    random.shuffle(points)
+    return points
+
+Log2ShuffledDistribution = ("Log2 Shuffled Distribution", generateLog2DistributionShuffled)
+
+def generateSquaredDistributionOrdered(N):
     points = [None] * N
     for i in range(N-1):
         points[i] = [i, i * i]
     points[N-1] = [N-1, 0]
     return points
 
-SquaredDistribution = ("Squared Distribution", generateSquaredDistribution)
+SquaredOrderedDistribution = ("Squared Ordered Distribution", generateSquaredDistributionOrdered)
+
+def generateSquaredDistributionInverted(N):
+    points = generateSquaredDistributionOrdered(N)
+    points.reverse()
+    return points
+
+SquaredInvertedDistribution = ("Squared Inverted Distribution", generateSquaredDistributionInverted)
+
+def generateSquaredDistributionShuffled(N):
+    points = generateSquaredDistributionOrdered(N)
+    random.shuffle(points)
+    return points
+
+SquaredShuffledDistribution = ("Squared Shuffled Distribution", generateSquaredDistributionShuffled)

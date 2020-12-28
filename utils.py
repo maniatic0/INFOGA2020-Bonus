179bd57c33c28683f2bec59461a3c70b9698e54d
Christian Oliveros <christianol_01@hotmail.com>
2020-12-28 13:43:29 -0400

    Cross Util

--- utils.py
+++ utils.py
@@ -6,12 +6,13 @@ Created on Tue Dec 26 16:32:33 2020
 """
 import numpy as np
 
-def is_right_turn(p0, p1, p2):
+def cross2DPoints(p0, p1, p2):
     e0 = [p1[0] - p0[0], p1[1] - p0[1]]
     e1 = [p2[0] - p1[0], p2[1] - p1[1]]
-    return np.cross(e0, e1) < 0
+    return np.cross(e0, e1)
+
+def is_right_turn(p0, p1, p2):
+    return cross2DPoints(p0, p1, p2) < 0
 
 def is_left_turn(p0, p1, p2):
-    e0 = [p1[0] - p0[0], p1[1] - p0[1]]
-    e1 = [p2[0] - p1[0], p2[1] - p1[1]]
-    return np.cross(e0, e1) > 0
+    return cross2DPoints(p0, p1, p2) > 0
